49588f5b70d3127397560e4dc4dda3ac63714e0f
Marius Comiati <marius.comiati@yahoo.com>
2016-11-13 00:29:43 +0200

    New statement not finished

---
+++ src/main/java/model/NewStm.java
@@ -0,0 +1,31 @@
+package model;
+
+import utils.MyIDictionary;
+import utils.MyIHeap;
+
+/**
+ * Created by Wyking on 11/9/2016.
+ */
+public class NewStm implements IStm {
+
+    String var_name;
+    Exp expression;
+
+    public NewStm(String var_name, Exp expression) {
+        this.var_name = var_name;
+        this.expression = expression;
+    }
+
+    public PrgState execute(PrgState state) throws Exception {
+        MyIHeap<Integer> heap = state.getExHeap();
+        MyIDictionary<String, Integer> simbols = state.getExDict();
+
+        int exp = expression.eval(simbols);
+        heap.put(exp);
+        return state;
+    }
+
+    public String toString(){
+        return null;
+    }
+}
